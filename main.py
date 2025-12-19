9b5132b77aaead976e7977f8985e4e89da34cbaf
SamuelCardosofc <samuelrochafc@gmail.com>
2025-12-18 22:12:48 -0300

    feat: finaliza cadastro de alunos e organiza estrutura do projeto

--- main.py
+++ main.py
@@ -35,20 +35,20 @@ alunos = []
 alunos.append({
     "RM": 123,
     "nome": "Ana",
-    "nota": [7.5, 8.0, 9.0]
+    "notas": [7.5, 8.0, 9.0]
 })
 
 alunos.append({
     "RM": 456,
     "nome": "Beatriz",
-    "nota": [7.5, 8.0, 9.0]
+    "notas": [7.5, 8.0, 9.0]
 })
 
 
 def cadastrar_aluno():
     RM = None
     nome = None
-    notas = None
+    notas = []
     nota = None
     erro = ""
     jaTem = False
@@ -67,12 +67,14 @@ def cadastrar_aluno():
             print("1 - Cadastrar aluno")
             if erro != "":
                 print(erro)
+                erro = ""
 
             if RM == None:
+                #validações do RM
                 try:
                     RM = float(input("Digite o RM do aluno: "))
                 except:
-                    erro = "Digite um número. Tente novamente."
+                    erro = "Digite um número pro RM. Tente novamente."
                     RM = None
 
                 if type(RM) == float:
@@ -88,44 +90,84 @@ def cadastrar_aluno():
                                 break
                         if not jaTem:
                             aluno["RM"] = RM
+                        else:
+                            jaTem = False
             else:
                 print("Digite o RM do aluno: %d" % aluno["RM"])
-                break
+
+                if nome == None :
+                    #validações do nome
+
+                    nome = input("Digite o nome do aluno: ")
+                    aluno["nome"] = nome
+
+                else:
+                    print("Digite o nome do aluno: %s" % aluno["nome"])
+
+                    if notas:
+                        print("Notas do aluno: ", end=" ")
+                        for i in range(0, len(notas)):
+                            if i == len(notas)-1:
+                                print(notas[i])
+                            else:
+                                print(notas[i], end=", ")
+
+                    nota = input("Digite a nota do aluno ou 'n' para parar: ")
+
+                    if nota.lower() == "n":
+                        break
+
+                    try:
+                        nota = float(nota)
+
+                        if nota < 0 or nota > 10:
+                            erro = "Digite uma nota válida (0 a 10)."
+                        else:
+                            notas.append(nota)
+                            aluno["notas"] = notas
+
+                    except:
+                        erro = "Entrada inválida. Digite um número ou 'n'."
+        alunos.append(aluno)
         break
+    main()
 
 # ===========================
 # EXECUÇÃO
 # ===========================
 
-while True:
-    limpar_tela()
-    mostrar_menu()
-    escolhaMenu = int(input("Digite o número da ação escolhida: "))
-    if escolhaMenu == 0:
-        encerrar()
-        break
-    elif escolhaMenu == 1:
-        cadastrar_aluno()
-        break
-    elif escolhaMenu == 2:
-        limpar_tela()
-        print("2 - Listar alunos")
-        break
-    elif escolhaMenu == 3:
-        limpar_tela()
-        print("3 - Buscar aluno")
-        break
-    elif escolhaMenu == 4:
-        limpar_tela()
-        print("4 - Remover aluno")
-        break
-    elif escolhaMenu == 5:
-        limpar_tela()
-        print("5 - Média das notas")
-        break
-    else:
-        print("Opção inválida! Tente novamente.")
+def main():
+    while True:
 
+        limpar_tela()
+        mostrar_menu()
 
+        try:
+            escolhaMenu = int(input("Digite o número da ação escolhida: "))
 
+            if escolhaMenu == 0:
+                encerrar()
+                break
+            elif escolhaMenu == 1:
+                cadastrar_aluno()
+                break
+            elif escolhaMenu == 2:
+                limpar_tela()
+                print("2 - Listar alunos")
+                break
+            elif escolhaMenu == 3:
+                limpar_tela()
+                print("3 - Buscar aluno")
+                break
+            elif escolhaMenu == 4:
+                limpar_tela()
+                print("4 - Remover aluno")
+                break
+            elif escolhaMenu == 5:
+                limpar_tela()
+                print("5 - Média das notas")
+                break
+        except:
+            print("erro")
 
+main()
